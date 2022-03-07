13d9fe4d83d109df77a5967e701557cd3634bc5e
Sasha9a <aleksandr_dovgiyy@mail.ru>
2022-03-07 19:42:30 +0300

    Тесты с ботом

--- src/client/index.ts
+++ src/client/index.ts
@@ -15,9 +15,18 @@ mp.events.add('playerReady', () => {
 });
 
 mp.events.add('PED:giveWeapon', (ped: PedMp) => {
-	ped.giveWeapon(mp.game.joaat('weapon_assaultrifle_mk2'), 1000, true);
+	mp.game.weapon.giveWeaponObjectToPed(mp.game.joaat('weapon_assaultrifle_mk2'), ped.id);
 });
 
 mp.events.add('PED:setArmour', (ped: PedMp) => {
 	ped.setArmour(100);
+});
+
+mp.events.add('render', () => {
+	mp.peds.forEach((ped) => {
+		if (ped.doesExist()) {
+			mp.game.invoke("0xBF0FD6E56C964FCB", ped.handle,  mp.game.joaat("weapon_assaultrifle_mk2"), 100, 0, 1);
+			mp.game.invoke("0x428CA6DBD1094446", ped.handle, 0);
+		}
+	});
 });
--- src/server/events.ts
+++ src/server/events.ts
@@ -5,12 +5,14 @@ mp.events.add('playerJoin', (player) => {
 
 	player.giveWeapon(mp.joaat('weapon_assaultrifle_mk2'), 1000);
 
-	const ped = mp.peds.new(mp.joaat('s_m_y_blackops_01'), new mp.Vector3(86.986, -2587.935, 6.005), {
-		frozen: false,
-		invincible: false,
-		dynamic: true
-	});
-	ped.controller = player;
-	player.call('PED:setArmour', [ped]);
-	player.call('PED:giveWeapon', [ped]);
+	for (let i = 0; i != 5; i++) {
+		const ped = mp.peds.new(mp.joaat('s_m_y_blackops_01'), new mp.Vector3(86.986 + i, -2587.935, 6.005), {
+			frozen: false,
+			invincible: false,
+			dynamic: true
+		});
+		ped.controller = player;
+		player.call('PED:setArmour', [ped]);
+		player.call('PED:giveWeapon', [ped]);
+	}
 });
